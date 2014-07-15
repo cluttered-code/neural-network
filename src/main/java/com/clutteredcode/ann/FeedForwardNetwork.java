43864ab4aa4bce70b0fd584e5c21af6d76c96b1f
David Clutter <cluttered.code@gmail.com>
2014-07-15 16:18:50 -0600

    Add some things for back propagation

--- src/main/java/com/clutteredcode/ann/FeedForwardNetwork.java
+++ src/main/java/com/clutteredcode/ann/FeedForwardNetwork.java
@@ -15,6 +15,8 @@
  */
 package com.clutteredcode.ann;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -34,6 +36,42 @@ public class FeedForwardNetwork {
         this.layers = layers;
     }
 
+    public void backPropagationTraining(final double[][] inputs, final double[][] expectedOutputs) {
+        // Guard Clause: input and output size mismatch
+        if (inputs.length != expectedOutputs.length)
+            throw new IllegalArgumentException("inputs (" + inputs.length + ") and expectedOutputs (" + expectedOutputs.length + ") must have the same number of elements");
+
+        // Loop each set of inputs
+        for (int i = 0; i < inputs.length; ++i) {
+
+            // Run one set of inputs and collect outputs for each layer
+            List<double[]> layerOutputs = new ArrayList<>();
+            fireAndCollectOutputs(inputs[i], layerOutputs);
+
+            // Reverse loop each neuron layer except input layer
+            List<double[]> layerDeltas = new LinkedList<>();
+            for (int j = layerOutputs.size() - 1; j > 0; --j) {
+
+                // Loop each neuron output in layer
+                double[] deltas = new double[layerOutputs.get(j).length];
+                for (int k = 0; k < deltas.length; ++k) {
+
+                    // Output layer propagation
+                    if (layerDeltas.isEmpty()) {
+                        deltas[k] = expectedOutputs[i][k] - layerOutputs.get(j)[k];
+                    }
+
+                    // Hidden layer propagation
+                    else {
+                        // TODO: hidden layer back propagation
+                    }
+                }
+                layerDeltas.add(deltas);
+
+            }
+        }
+    }
+
     /**
      * Fire each {@link com.clutteredcode.ann.FeedForwardLayer}, piping the output of the previous into the next.
      *
@@ -41,11 +79,18 @@ public class FeedForwardNetwork {
      * @return The outputs of the last {@link com.clutteredcode.ann.FeedForwardLayer}.
      */
     public double[] fire(final double[] inputs) {
-        double[] currentInputs = inputs;
+        return fireAndCollectOutputs(inputs, new ArrayList<>());
+    }
 
-        for (final FeedForwardLayer layer : layers)
-            currentInputs = layer.fire(currentInputs);
+    protected double[] fireAndCollectOutputs(final double[] inputs, List<double[]> layerOutputs) {
+        layerOutputs.add(inputs);
 
-        return currentInputs;
+        for (final FeedForwardLayer layer : layers) {
+            final double[] input = layerOutputs.get(layerOutputs.size() - 1);
+            final double[] output = layer.fire(input);
+            layerOutputs.add(output);
+        }
+
+        return layerOutputs.get(layerOutputs.size() - 1);
     }
 }
